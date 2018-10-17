05c497f786a2b7abacdba406e40fb1625bd6b1a7
Delon <delon.vandeventer@medavie.bluecross.ca>
2018-10-17 13:25:41 -0300

    Using api

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,5 +1,5 @@
 <template>
-      <div class="content">
+      <div v-if="availableParts" class="content">
         <div class="preview">
           <CollapsibleSection>
           <div class="preview-content">
@@ -58,13 +58,17 @@
 </template>
 
 <script>
-import availableParts from '../data/parts';
+// import availableParts from '../data/parts';
+
 import createdHookMixin from './created-hook-mixin';
 import PartSelector from './PartSelector.vue';
 import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  created() {
+    this.$store.dispatch('getParts');
+  },
   beforeRouteLeave(to, from, next) {
     if (this.addedToCart) {
       next(true);
@@ -78,7 +82,6 @@ export default {
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
-      availableParts,
       addedToCart: false,
       cart: [],
       selectedRobot: {
@@ -92,6 +95,9 @@ export default {
   },
   mixins: [createdHookMixin],
   computed: {
+    availableParts() {
+      return this.$store.state.parts;
+    },
     saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
     },
@@ -110,7 +116,7 @@ export default {
         robot.rightArm.cost +
         robot.torso.cost +
         robot.base.cost;
-      this.$store.commit('addRobotToCart', Object.assign({}, robot, { cost }));
+      this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }));
       this.addedToCart = true;
     },
   },
